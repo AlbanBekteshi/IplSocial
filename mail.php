53b3df773d27e242d5d445c66e1426ca54924047
Alban <albanbekteshi97@gmail.com>
2021-01-19 09:44:50 +0100

    premier commit

---
+++ mail.php
@@ -0,0 +1,12 @@
+<div id="contenu">
+    <h2>IPL Social </h2>
+
+    <div class="formulaire">
+        <form action="/action_page.php">
+            <label for="email">email:</label><br>
+            <input type="text" id="email" name="email"><br>
+            <input type="submit" value="Envoyer">
+        </form>
+    </div>
+
+</div><!-- #contenu -->
